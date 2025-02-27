dcb7bf175f70516f59a41b1255669ca63db35e1a
Mershad <mehrshadalipoor77@gmail.com>
2025-02-27 16:34:58 +0100

    hmm

---
+++ session2.py
@@ -0,0 +1,20 @@
+# ================================================================================================
+# TYPE CONVERSION
+x = input("x: ")
+y = int(x) + 1
+
+print(f"x: {x}, y: {y}")
+
+# int(x)
+# float(x)    # Convert to float
+# bool(x)     # Convert to boolean
+# str(x)      # Convert to string
+
+# ================================================================================================
+# Falsy values
+# ""    # Empty string
+# 0     # Zero
+# None  # None
+# False # False
+# bool("False")  # True
+# bool("") -->   # False
--- session5.py
+++ session5.py
@@ -46,3 +46,23 @@
 
 # git branch feature-login
 # git checkout feature-login
+
+#================================================================================================
+
+# git remote
+# what is a remote in git?
+    # A remote in git is a copy of the repository that is stored on a different server.
+    # It allows you to collaborate with other developers by sharing your code.
+    # You can push your changes to the remote repository and pull changes from it.
+    # The default name of the remote repository on github is "origin".
+
+# process of working with remotes:
+    # code -> git add -> staging area -> git commit -> local repository -> 
+    # git push -> remote repository -> git pull -> local repository
+
+# git commands for remotes:
+    # git remote = list all remotes
+    # git remote -v = list all remotes with URLs
+    # git remote add <name> <url> = add a new remote
+    # git remote remove <name> = remove a remote
+    # git remote rename <old_name> <new_name> = rename a remote 
